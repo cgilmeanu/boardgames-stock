5ac84c87882893d83d7152f46ead1f1fff1d4e68
Claudiu Gilmeanu <claudiu.gilmeanu@endava.com>
2019-03-16 21:21:46 +0200

    setting up app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,33 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<v-app>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-start  justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Boardgames Stock</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+      <router-view/>
+    </v-content>
+</v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+      //
     }
   }
 }
-</style>
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+import 'vue-material-design-icons/styles.css';
 
 Vue.config.productionTip = false;
 
