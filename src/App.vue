5549b0843ed5b1562d83d3d689526b8b1decb634
Claudiu Gilmeanu <claudiu.gilmeanu@endava.com>
2019-03-17 21:49:14 +0200

    Played with v-list from vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,44 @@
 <template>
-<v-app>
+  <v-app>
     <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-start  justify-center>
-          <v-flex xs12 sm8 md4>
+      <v-container fluid>
+        <v-layout align-start justify-center>
+          <v-flex xs12 sm12 md8>
             <v-card class="elevation-12">
               <v-toolbar dark color="primary">
                 <v-toolbar-title>Boardgames Stock</v-toolbar-title>
                 <v-spacer></v-spacer>
+                <v-tabs class="tabs" right dark color="primary" slider-color="accent">
+                  <v-tab grow="false" :to="{ name: 'home'}">Home</v-tab>
+                  <v-tab grow="false" :to="{ name: 'about'}">About</v-tab>
+                </v-tabs>
               </v-toolbar>
             </v-card>
           </v-flex>
         </v-layout>
+        <v-layout align-start justify-center>
+          <v-flex xs12 sm12 md8>
+            <router-view/>
+          </v-flex>
+        </v-layout>
       </v-container>
-      <router-view/>
     </v-content>
-</v-app>
+    <v-footer>
+      <v-layout justify-center>Copyright (c), 2019</v-layout>
+    </v-footer>
+  </v-app>
 </template>
-
 <script>
-
 export default {
   name: 'App',
-  components: {
-  },
-  data () {
-    return {
-      //
-    }
-  }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '@/styles/global.scss';
+
+.tabs {
+  max-width: 50%;
+}
+</style>
+
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,55 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-content>
+    <v-layout justify-center row>
+      <v-flex class="headline" pt-4 text-xs-center>A simple and lightweight boardgames shelf</v-flex>
+    </v-layout>
+    <v-layout justify-center row>
+      <v-flex class="title" pt-5 text-xs-center>List of technologies</v-flex>
+    </v-layout>
+    <v-container grid-list-sm fluid>
+      <v-layout justify-center row wrap>
+        <v-list two-line>
+          <template v-for="(tech, index) in technologyStack">
+            <v-divider v-bind:key="index" v-if="tech.divider"></v-divider>
+            <v-list-tile v-else v-bind:key="tech.title" @click.prevent="navigateTo(tech.link)">
+              <v-list-tile-content>
+                <v-list-tile-title class="title">{{ tech.title }}</v-list-tile-title>
+                <v-list-tile-sub-title class="subheading">{{ tech.subtitle }}</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+        </v-list>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+import router from '@/router'
+
+export default Vue.extend({
+  data: () => {
+    return {
+      technologyStack: [
+        {
+          title: 'Vue',
+          subtitle: 'A progressive framework for building user interfaces',
+          link: 'https://vuejs.org/'
+        },
+        { divider: true },
+        {
+          title: 'Vuetify',
+          subtitle: 'Material Design Component Framework',
+          link: 'https://vuetifyjs.com'
+        }
+      ]
+    }
+  },
+  methods: {
+    navigateTo(link: string) {
+      window.open(link, '_blank')
+    }
+  }
+})
+</script>
+
